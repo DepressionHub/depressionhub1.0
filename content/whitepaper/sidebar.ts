c3a429f3e140d5a1d68cb82b97045f5dbe40fce6
TANMAY <tanmay@TANMAYs-MacBook-Air.local>
2024-05-16 16:14:48 +0530

    remove the unknow part of an whitepaper

--- content/whitepaper/sidebar.ts
+++ content/whitepaper/sidebar.ts
@@ -2,17 +2,4 @@ import { SidebarDefinition } from "@/lib/markdown";
 
 export const sidebar: SidebarDefinition = [
   "introduction",
-  {
-    label: "Protocol",
-    items: [
-      "protocol/accounts",
-      "protocol/crypto-primitives",
-      "protocol/transactions",
-      "protocol/consensus",
-      "protocol/blocks",
-    ],
-  },
-  "networking",
-  "glossary",
-  "governance",
 ];

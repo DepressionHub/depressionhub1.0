165bc8ab574a680aad200321bc959371ce7f5364
ShubhamMewara <shubhammewara12@gmail.com>
2024-05-16 17:49:04 +0530

    yarn

--- shared/constants.ts
+++ shared/constants.ts
@@ -8,6 +8,3 @@ const SOCIAL_LINKS = {
 export const CONSTANTS = {
   SOCIAL_LINKS,
 };
-
-export const GRANTS_FORM_URL =
-  "https://docs.google.com/forms/d/e/1FAIpQLSf5ws1lLHg7aao87744Us7wO7dADvPiGl2ZEBPRBnDB-mXiAg/viewform";

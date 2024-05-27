096d68a8cab1c2b700f100749e052657615db626
TANMAY <tanmay@TANMAYs-MacBook-Air.local>
2024-05-27 17:47:48 +0530

    added some stuff

--- backend/src/utils/authUtils.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-exports.generateToken = (userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: '1h',
-  });
-
-  return token;
-};
